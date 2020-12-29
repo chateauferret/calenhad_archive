5d8bf41d808630e8decab9db00fb204729abb155
chateauferret <37Polecats>
2020-12-29 00:50:41 +0000

    Exclude the build directory from the repository.

---
+++ src/modules/Findlibs2.cmake
@@ -0,0 +1,26 @@
+# - Try to find LibS2
+# Once done this will define
+#  LIBS2_FOUND - System has LibXml2
+#  LIBS2_INCLUDE_DIRS - The LibXml2 include directories
+#  LIBS2_LIBRARIES - The libraries needed to use LibXml2
+#  LIBS2_DEFINITIONS - Compiler switches required for using LibXml2
+
+find_package(PkgConfig)
+pkg_check_modules(PC_LIBS2 QUIET libs2)
+set(LIBS2_DEFINITIONS ${PC_LIBS2_CFLAGS_OTHER})
+
+find_path(LIBS2_INCLUDE_DIR NAMES s2/_fp_contract_off.h)
+
+find_library(LIBS2_LIBRARY NAMES s2 libs2
+        HINTS ${PC_LIBS2_LIBDIR} ${PC_LIBS2_LIBRARY_DIRS} )
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set LIBXML2_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(libs2  DEFAULT_MSG
+        LIBS2_LIBRARY LIBS2_INCLUDE_DIR)
+
+mark_as_advanced(LIBS2_INCLUDE_DIR LIBS2_LIBRARY )
+
+set(LIBXML2_LIBRARIES ${LIBS2_LIBRARY} )
+set(LIBXML2_INCLUDE_DIRS ${LIBS2_INCLUDE_DIR} )
